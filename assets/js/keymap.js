5926c22da37d910a24fd28b63434755d49eab483
Mackenzie Stewart <github@macstew.art>
2024-02-11 15:31:00 -0500

    nested keymap node traversal

---
+++ assets/js/keymap.js
@@ -0,0 +1,22 @@
+const KEYMAP = {
+    children: {
+        q: {
+            name: 'Current Events',
+            link: 'https://en.wikipedia.org/wiki/Portal:Current_events',
+            children: {
+                w: {
+                    name: 'Outlook',
+                    link: 'https://outlook.live.com/mail/0/inbox',
+                },
+                s: {
+                    name: 'Calendar',
+                    link: 'https://calendar.google.com/calendar',
+                },
+            },
+        },
+        a: {
+            name: 'Hackernews',
+            link: 'https://news.ycombinator.com/',
+        }
+    }
+}
--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,38 +1,83 @@
+// const registerKey = (key, url) => {
+//     KEYMAP.set(key, url);
+// }
 
-const keys = new Map();
-
-const registerKey = (key, url) => {
-    keys.set(key, url);
-}
+var keySeq = []
+var nodeSeq = [KEYMAP]
+var curr = nodeSeq[0]
 
 const handleKey = (e) => {
+    console.log(keySeq)
+    // console.log(nodeSeq)
+    // console.log(curr)
     var key = e.key.toLowerCase()
-    console.log(key);
-    if (keys.has(key)) {
-        if (e.shiftKey) {
-            window.open(keys.get(key), '_blank');
-        } else {
-            window.open(keys.get(key), '_self');
-        }
+    if (key === 'escape') {
+        back()
+        return
+    }
+    if ('children' in curr && key in curr.children) {
+        console.log("adding key to sequence")
+        keySeq.push(key)
+        curr = curr.children[key]
+        nodeSeq.push(curr)
+        console.log("new seq: " + keySeq)
+        // console.log(keySeq)
+        // node = node.children.get(key);
+        // console.log(nodeSeq)
+        // console.log(sequence);
+        // if (e.shiftKey) {
+        //     window.open(node.get(key)?.link, '_blank');
+        // } else {
+        //     window.open(node.get(key)?.link, '_self');
+        // }
     }
 }
 
 document.onkeydown = function(e) {
-    handleKey(e);
-};
+    handleKey(e)
+}
 
-for (const item of CONFIG.firstlistsContainer) {
-    for (const link of item.links) {
-        if (link.key) {
-            registerKey(link.key, link.link);
-        }
+go = (e, link) => {
+    if (e.shiftKey) {
+        window.open(link, '_blank')
+    } else {
+        window.open(link, '_self')
     }
 }
 
-for (const item of CONFIG.secondListsContainer) {
-    for (const link of item.links) {
-        if (link.key) {
-            registerKey(link.key, link.link);
-        }
-    }
+step = (key) => {
+    keySeq.push(key)
+    nodeSeq.push(curr.children[key])
+    curr = nodeSeq[-1]
 }
+
+back = () => {
+    if (keySeq.length === 0) {
+        return
+    }
+    keySeq.pop()
+    nodeSeq.pop()
+    curr = nodeSeq[nodeSeq.length - 1]
+}
+
+reset = () => {
+    keySeq.splice(0)
+    nodeSeq.splice(1)
+    curr = nodeSeq[0]
+}
+
+// for (const item of CONFIG.firstlistsContainer) {
+//     for (const link of item.links) {
+// if (link.key) {
+//     registerKey(link.key, link.link);
+//     }
+// }
+// }
+
+// for (const item of CONFIG.secondListsContainer) {
+//     for (const link of item.links) {
+// if (link.key) {
+//     registerKey(link.key, link.link);
+//     }
+// }
+// }
